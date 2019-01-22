2e582111a5aee04c2c1aeb4b3b944bbfee096506
lucaspchartier <lucaspchartier@gmail.com>
2019-01-22 11:20:42 -0500

    Created other hotel components for different CRUD actions.

---
+++ src/hotels/api.js
@@ -0,0 +1,71 @@
+const apiUrl = 'http://localhost:4741'
+
+export const handleErrors = res => {
+  if (res.ok) {
+    return res
+  } else  {
+    throw new Error('Recieved status in 400 or 500 range.')
+  }
+}
+
+export const createHotel = (user, data) => {
+  return fetch(apiUrl + '/hotels', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization':`Token token=${user.token}`
+    },
+    body: JSON.stringify({
+      hotel: {
+        name: data.hotel.name,
+        location: data.hotel.location
+      }
+    })
+  })
+}
+
+export const getHotels = user => {
+  return fetch(apiUrl + '/hotels', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization':`Token token=${user.token}`
+    }
+  })
+}
+
+export const showHotel = user => {
+  return fetch(apiUrl + `/hotels/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization':`Token token=${user.token}`
+    }
+  })
+}
+
+export const updateHotel = (user, data) => {
+  return fetch(apiUrl + `/hotels/${id}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization':`Token token=${user.token}`
+    },
+    body: JSON.stringify({
+      hotel: {
+        name: data.hotel.name,
+        location: data.hotel.location
+      }
+    })
+  })
+}
+
+export const deleteHotel = user => {
+  return fetch(apiUrl + `/hotels/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization':`Token token=${user.token}`
+    }
+  })
+}
