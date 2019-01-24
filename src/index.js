8340632f61abb1b00d5211a68cd9650152cb869d
lucaspchartier <lucaspchartier@gmail.com>
2019-01-24 13:36:40 -0500

    Removed index.scss file as it was empty and not being used whatsoever.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import { HashRouter } from 'react-router-dom'
