eafe7a40bd5125db37c1e672b85c623e68c0180d
lucaspchartier <lucaspchartier@gmail.com>
2019-01-24 08:43:01 -0500

    Added feedback messages when a user gets all of the hotels as well as when a user clicks on just one hotel.

--- src/hotels/messages.js
+++ src/hotels/messages.js
@@ -1,6 +1,10 @@
 export default {
   createHotelSuccess: 'You successfully added a hotel!',
   createHotelFailure: 'Error: Something went wrong. Please try again.',
+  getHotelsSuccess: 'Here are your hotels so far!',
+  getHotelsFailure: 'Error: Something went wrong. Please try again.',
+  showHotelSuccess: 'Here\'s your hotel!',
+  showHotelFailure: 'Error: Something went wrong. Please try again.',
   updateHotelSuccess: 'You successfully edited your hotel!',
   updateHotelFailure: 'Error: Something went wrong. Please try again.',
   deleteHotelSuccess: 'You successfully deleted your hotel!',
