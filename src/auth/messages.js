9ff55ab01e7c591162a793bfabdb41b8d29e6a13
lucaspchartier <lucaspchartier@gmail.com>
2019-01-24 15:03:05 -0500

    Changed sign out message to make more sense to the user.

--- src/auth/messages.js
+++ src/auth/messages.js
@@ -3,7 +3,7 @@ export default {
   signUpFailure: 'Error: Something went wrong. Please try again.',
   signInSuccess: 'Welcome! It\'s great to see you!',
   signInFailure: 'Error: Something went wrong. Please try again.',
-  signOutSuccess: 'Hope to see you again shortly!',
+  signOutSuccess: 'Hope to see you again soon!',
   changePasswordSuccess: 'You successfully changed passwords!',
   changePasswordFailure: 'Error: Something went wrong. Please try again.'
 }
